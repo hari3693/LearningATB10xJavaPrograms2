016c51a0e28cdd760108fcb9555b5ac5ad49aad6
Hari <kumar.hari324@gmail.com>
2025-01-15 09:26:56 +0530

    program for 9 table

---
+++ src/program_for_9_table.java
@@ -0,0 +1,12 @@
+public class program_for_9_table {
+    public static void main(String[] args) {
+        int num = 9;
+        int out = 0;
+        for (int i = 1; i <= 10; i++) {
+            out = num * i;
+            System.out.println(num + " * " + i + " = " + out);
+
+        }
+
+    }
+}
